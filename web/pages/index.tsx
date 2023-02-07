3b43bb46c947e7910bca974717262deb4ad35bd5
AlexSouza645 <59890212+AlexSouza645@users.noreply.github.com>
2023-02-07 11:05:14 -0300

    requisições

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,11 +1,19 @@
-
-
-export default function Home() {
+export default function Home(props) {
   return (
-    <div>
-     
-      <h1>hello world </h1>
-    </div>
-      
-  )
+    
+      <h1>Contagem : {props.count} </h1>
+    
+  );
 }
+export const getServerSidePops = async () => {
+  const response = await fetch("http://localhost:3333/pools/count");
+  const data = await response.json();
+  //.then(response => response.json())//retorna em formato json
+  //.then(data =>{
+  console.log(data); //busca os dados e verifica se os dados foram buscados corretamente
+  return {
+    props: {
+      count: data.count,
+    },
+  };
+};
