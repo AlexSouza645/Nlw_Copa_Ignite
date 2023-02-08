e4376ccf92bd699d97b1af5b1095be76f40a3f39
AlexSouza645 <59890212+AlexSouza645@users.noreply.github.com>
2023-02-08 11:06:51 -0300

    fix

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,9 +1,10 @@
-export default function Home(props) {
-  return (
-    
-      <h1>Contagem : {props.count} </h1>
-    
-  );
+interface HomeProps{
+  count: number;
+}
+
+
+export default function Home(props: HomeProps) {
+  return <h1>Contagem : {props.count}</h1>;
 }
 export const getServerSidePops = async () => {
   const response = await fetch("http://localhost:3333/pools/count");
