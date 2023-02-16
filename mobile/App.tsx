d817a781f6f401a6d7dbee559480c46b097ee5bc
AlexSouza645 <59890212+AlexSouza645@users.noreply.github.com>
2023-02-16 10:51:38 -0300

    setup nativeBase

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,24 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 
+import { NativeBaseProvider,Text,  Center } from "native-base";
+// /* View style={styles.container} */ alignItems='center' justifyContent='center'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Center flex={1} bgColor="indigo.700">
+        <Text color="yellow " fontSize={24}>
+          Hello World!
+        </Text>
+        <StatusBar style="auto" />
+      </Center>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#000",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    color: '#FFF',
-    fontSize: 24
-  },
-});
