0baf5c8a1888e21f38d4cc97ed361d63dad5a722
AlexSouza645 <59890212+AlexSouza645@users.noreply.github.com>
2023-02-20 12:32:11 -0300

    criando tabelas

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,15 @@
-import { StatusBar } from "expo-status-bar";
 
-import { NativeBaseProvider, Text, Center } from "native-base";
+
+import { NativeBaseProvider, Center, StatusBar} from "native-base";
 // /* View style={styles.container} */ alignItems='center' justifyContent='center'
 import {THEME} from './src/styles/theme';
 import{useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold}  from '@expo-google-fonts/roboto'
 
 // importação do componente de loading
 import { Loading } from "./src/components/loading";
+
+import { SignIn } from "./src/screens/Signin";
+import { transparentize } from "native-base/lib/typescript/theme/tools";
 
 export default function App() {
   // saolvar num array para saber se a fonte foi carregada
@@ -16,12 +19,18 @@ const [fonstsLoaded]=useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bo
   return (
     <NativeBaseProvider theme={THEME}>
       <Center flex={1} bgColor="gray.600">
-        <Loading/>
+        <StatusBar barStyle="light-content" backgroundColor= 'transparent' translucent/>
+        {/* barStyle = barra de status ficar visivel
+         */}
+           
+     
+    {
+      fonstsLoaded ?  <SignIn/>: <Loading/> //se a fonte esta disponivel ela é exibida caso contrario mostra  o loading 
+    }
+ 
+        
        
       </Center>
     </NativeBaseProvider>
   );
 }
-{/* <Text color="warning.400" fontSize={24}> */}
-          // Hello World!
-        // </Text> <StatusBar style="auto" />
