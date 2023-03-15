00fd82db00202b5b3c30e31c974e0e62767f6e0c
alex miranda de souza <59890212+AlexSouza645@users.noreply.github.com>
2023-03-15 12:33:42 -0300

    mobile setup

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,36 +1,24 @@
-
-
-import { NativeBaseProvider, Center, StatusBar} from "native-base";
-// /* View style={styles.container} */ alignItems='center' justifyContent='center'
-import {THEME} from './src/styles/theme';
-import{useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold}  from '@expo-google-fonts/roboto'
-
-// importação do componente de loading
-import { Loading } from "./src/components/loading";
-
-import { SignIn } from "./src/screens/Signin";
-import { transparentize } from "native-base/lib/typescript/theme/tools";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  // saolvar num array para saber se a fonte foi carregada
-const [fonstsLoaded]=useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold}); 
-
-
   return (
-    <NativeBaseProvider theme={THEME}>
-      <Center flex={1} bgColor="gray.600">
-        <StatusBar barStyle="light-content" backgroundColor= 'transparent' translucent/>
-        {/* barStyle = barra de status ficar visivel
-         */}
-           
-     
-    {
-      fonstsLoaded ?  <SignIn/>: <Loading/> //se a fonte esta disponivel ela é exibida caso contrario mostra  o loading 
-    }
- 
-        
-       
-      </Center>
-    </NativeBaseProvider>
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello world !!!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 32
+  }
+});
