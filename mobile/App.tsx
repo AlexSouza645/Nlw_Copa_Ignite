416b1f31ab196a54aa1574ba647b874c4e0ee956
alex miranda de souza <59890212+AlexSouza645@users.noreply.github.com>
2023-03-17 15:55:53 -0300

    setup nativebase

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,24 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet,  View } from 'react-native';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+ 
+// importar temas
+import { THEME } from './src/styles/theme';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { Loading } from './src/components/loading';
+import { Signin } from './src/screens/signin';
+import { transparentize } from 'native-base/lib/typescript/theme/tools';
+
+
 
 export default function App() {
+
+  // saolvar num array para saber se a fonte foi carregada
+  const [fonstsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
+
+
+
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello world !!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+     <StatusBar barStyle='light-content' backgroundColor='transparent' translucent/>
+        {
+          fonstsLoaded ?<Signin/>: <Loading />
+          // se a fonte esta carregada aparece tela de signin se nao mostra o loading
+        }
+
+
+        
+     
+    </NativeBaseProvider>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#FFF',
-    fontSize: 32
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#000',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   title: {
+//     color: '#FFF',
+//     fontSize: 32
+//   }
+// });
---
+++ mobile/src/screens/signin.tsx
@@ -0,0 +1,14 @@
+import { Center, Text } from "native-base";
+
+
+export function Signin(){
+    return(
+        <Center flex='1' backgroundColor="gray.600" >
+       
+        <Text color="white" fontSize={26} fontFamily='Heading'>
+         SignIn
+        </Text>
+        
+      </Center>
+    )
+} 
