3ad58e9dd3ce7658143b394a4a0b8bc8d7cb8e57
alex miranda de souza <59890212+AlexSouza645@users.noreply.github.com>
2023-03-29 10:56:04 -0300

    Update index.tsx

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -4,18 +4,28 @@
 // interface HomeProps {
 //   count: number;
 // }
-
+import Image from 'next/image'
 import appPreviewImage from '../assets/app-nlw-copa.png'
+import logoImage from '../assets/logo.svg'
+import usersAvatar from '../assets/avatares.png'
 
 
-export default function Home( ) {
+export default function Home() {
 
   return (
     <div>
       <main>
 
+        <Image src={logoImage} alt='logo do app Nlw Copa' />
+        <h1 className='bg-white'>Crie seu próprio bolão da Copa, e compartilhe com os amigos </h1>
+  <div>
+    <Image src={usersAvatar} alt='imagme de avatares de usuários'/>
+  </div>
       </main>
-      <img src=" " alt="" />
+      <Image
+        src={appPreviewImage}
+        alt="Dois celulares exibindo uma prévia do app Nlw Copa"
+        quality={100} />
     </div>
   )
 }
