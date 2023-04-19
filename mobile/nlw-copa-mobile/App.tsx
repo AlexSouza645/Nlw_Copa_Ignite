527860a205c9bd97e75fd0da75427cde03e16635
alex miranda de souza <59890212+AlexSouza645@users.noreply.github.com>
2023-04-19 11:01:17 -0300

    botao loading

--- mobile/nlw-copa-mobile/App.tsx
+++ mobile/nlw-copa-mobile/App.tsx
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 // import { StyleSheet, View } from 'react-native';
-import { NativeBaseProvider,Text, VStack, Center } from 'native-base';
+import { NativeBaseProvider, Text, VStack, Center } from 'native-base';
 import { THEME } from './src/styles/theme';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+// importar  componentes
+import { Loading } from './src/components/loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
+
+
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <Center flex={1} bgColor={'gray.600'}>
+      <Center flex={1} bgColor={'gray.900'}>
+      <Loading />
         <Text color="white" fontSize={28}>Hello World </Text>
         <StatusBar style="auto" />
       </Center>
