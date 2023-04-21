72cf429cad67cecc5bf163ddf271eda0c32cb011
alex miranda de souza <59890212+AlexSouza645@users.noreply.github.com>
2023-04-21 12:27:07 -0300

    tela loading

--- mobile/nlw-copa-mobile/App.tsx
+++ mobile/nlw-copa-mobile/App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+
 // import { StyleSheet, View } from 'react-native';
 import { NativeBaseProvider, Text, VStack, Center } from 'native-base';
 import { THEME } from './src/styles/theme';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
 // importar  componentes
 import { Loading } from './src/components/loading';
-
+import { SignIn } from './src/screens/SignIn';
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
 
@@ -13,11 +15,11 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <Center flex={1} bgColor={'gray.900'}>
-      <Loading />
-        <Text color="white" fontSize={28}>Hello World </Text>
-        <StatusBar style="auto" />
-      </Center>
+  
+        {
+          fontsLoaded ? <SignIn /> : <Loading/>
+        }
+      
     </NativeBaseProvider>
 
   );
--- mobile/nlw-copa-mobile/src/components/loading.tsx
+++ mobile/nlw-copa-mobile/src/components/loading.tsx
@@ -4,9 +4,7 @@ export function Loading() {
     return (
         <Center flex={1} bg={"gray.900"}> 
 
-            <Spinner color="yellow.500">
-
-            </Spinner>
+         
         </Center>
     )
 }
