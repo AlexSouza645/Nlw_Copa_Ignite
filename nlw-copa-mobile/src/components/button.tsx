997e218c9b23ccd87970055663b67ac3f10331c7
alex miranda de souza <59890212+AlexSouza645@users.noreply.github.com>
2023-05-02 10:52:56 -0300

    componente button

---
+++ nlw-copa-mobile/src/components/button.tsx
@@ -0,0 +1,11 @@
+import { Button as ButtonNativeBase } from "native-base" //Button as ButtonNativeBase é usado para renomear componen
+
+export function Button() {
+    return (
+<ButtonNativeBase>
+
+
+    
+</ButtonNativeBase>
+    );
+}
