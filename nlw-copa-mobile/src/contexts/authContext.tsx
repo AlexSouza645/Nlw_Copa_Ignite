9dd427508329aa12af9d61c38fed5a806c7c7dbb
alex miranda de souza <59890212+AlexSouza645@users.noreply.github.com>
2023-05-13 11:48:08 -0300

    auth do google finalizado

--- nlw-copa-mobile/src/contexts/authContext.tsx
+++ nlw-copa-mobile/src/contexts/authContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, ReactNode, useEffect } from "react";
 import * as AuthSession from 'expo-auth-session'
 import * as WebBrowser from 'expo-web-browser'
 import * as Google from 'expo-auth-session/providers/google'
@@ -27,6 +27,8 @@ export const AuthContext = createContext({} as AuthContextDataProps)
 //criar uyma função que prove o contexto criado
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
+    // salvar o estado do usuario
+    const [user, setUser] = useState<UserProps>({} as UserProps)
 
     const [isUserLoading, setIsUserLoading] = useState(false)
 
@@ -44,17 +46,35 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
 
         try {
             setIsUserLoading(true)
+            await promptAsync()
+
 
         } catch (error) {
+            console.log(error)
+            throw error
 
         } finally { setIsUserLoading(false) }
     }
+
+    async function signInWithGoogle(access_token: string) {
+        console.log('token de autenticação ===>', access_token)
+    }
+
+
+    useEffect(() => {
+        if (response?.type === 'success' && response.authentication?.accessToken) {
+            signInWithGoogle(response.authentication.accessToken)
+        
+         }
+    }, [response])
+
+
     return (
         <AuthContext.Provider value={{
             signIn,
             isUserLoading,
-             user: { name: "Lucas", avatarUrl: "https://github.com/AlexSouza645.png" }
-
+            // user: { name: "Lucas", avatarUrl: "https://github.com/AlexSouza645.png" }
+            user
 
 
 
