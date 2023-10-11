d938cb7a4196113df5516b8e6fdc7afcd3945187
alex miranda de souza <59890212+AlexSouza645@users.noreply.github.com>
2023-10-11 10:42:00 -0300

    setup seed

---
+++ prisma/seed.ts
@@ -0,0 +1,77 @@
+import { PrismaClient } from "@prisma/client"
+
+// fazer conexão com banco de dados
+const prisma = new PrismaClient(
+
+)
+
+
+async function main() {
+    const user = await prisma.user.create({   //criar um novo usuario 
+        data: {
+            name: 'John Doe',
+            email: 'johndoe@gmail.com',
+            avatarUrl: 'https://github.com/AlexSouza645.png',
+
+        }
+    })
+
+    //  criar um bolao
+    const pool = await prisma.pool.create({
+        data: {
+
+            title: 'Example Pool',
+            code: 'BOL123',
+            userId: user.id,
+
+            participants: {
+                create: {
+                    userId: user.id
+                }
+            }
+
+
+
+        }
+
+
+
+    })
+    await prisma.games.create({
+        data: {
+            date: '2023-03-20T20:23:35.936Z',
+            firstTeamCountryCode: "DE",
+            secondTeamCountryCode: 'BR'
+        }
+    })
+
+    await prisma.games.create({
+        data: {
+            date: '2023-03-19T20:23:35.936Z',
+            firstTeamCountryCode: "BR",
+            secondTeamCountryCode: 'AR',
+
+
+            guesses:{
+                create:{
+                    firstTeamPoint:2,
+                    secondTeamPoint:1,
+
+                    participant:{
+                        connect:{
+                            userId_poolId:{
+                                userId: user.id,
+                                poolId: pool.id
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
+
+
+
+main()
