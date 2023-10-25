074aab5614cc0fdd4043eb12b20ce8a0383ab1aa
alex miranda de souza <59890212+AlexSouza645@users.noreply.github.com>
2023-10-25 10:24:26 -0300

    craindo rotas de nauth google

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,41 @@
 import { FastifyInstance } from "fastify"
 import { prisma } from "../lib/prisma"
+import { z } from "zod"
 
 
-export async function authRoutes(fastify:FastifyInstance){
-    
+export async function authRoutes(fastify: FastifyInstance) {
+
+    fastify.post("/users", async (request) => {
+        const createUserBody = z.object({
+            access_token: z.string()
+        })
+        const { access_token } = createUserBody.parse(request.body)
+
+
+        // comunicacao com api do google
+        const userResponse = await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer${access_token}`
+            }
+        })
+
+        // retornara algumas informações
+
+        const userData = await userResponse.json()
+
+        const userInfoSchema = z.object({
+            id: z.string(),
+            email: z.string().email(),
+            name: z.string(),
+            picture: z.string().url()
+        })
+        // validação de dados 
+        const userInfo = userInfoSchema.parse(userData)
+
+        return { userInfo }
+
+
+    })
+
 }
