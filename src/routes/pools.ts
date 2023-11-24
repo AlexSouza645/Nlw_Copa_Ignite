af3e14707a6c4de96a0a70ab35a7a0112647d463
alex miranda de souza <59890212+AlexSouza645@users.noreply.github.com>
2023-11-24 10:34:51 -0300

    novas requests

--- src/routes/pools.ts
+++ src/routes/pools.ts
@@ -67,19 +67,72 @@ export async function poolRoutes(fastify: FastifyInstance) {
         //rota para entrar em um bolao
         fastify.post('/pools/:id/join',
             { onRequest: [authenticate] },
-            (request, reply) => { 
-                const joinPoolBody=z.object({
-                    code:z.string()
+            async (request, reply) => {
+                const joinPoolBody = z.object({
+                    code: z.string()
                 })
-                const{code}= joinPoolBody.parse(request.body)
+                const { code } = joinPoolBody.parse(request.body)
             })
 
+        fastify.get('/pools', {
+
+            onRequest: [authenticate]
+        }, async (request) => {
+            const pools = await prisma.pool.findMany({
+                where: {
+                    participants: {
+                        some: {
+                            userId: request.user.sub
+                        }
+                    }
+                }
+            })
+            return(pools)
+        })
 
 
+        // validação antes de criar o participante
+        const pool = await prisma.pool.findUnique({
+            where: {
+                code,
+            }, include: {
+                participants: {
+                    where: {
+                        userId: request.user.sub,
+                    }
+                }
+            }
+        })
+        if (!pool) {
+            return reply.status(400).send({
+                message: "pool not found"
+            })
+        }
+        if (pool.participants.length > 0) {
+            return reply.status(400).send({
+                message: "you already joined this pool"
+            })
 
+        }
 
+        if (!pool.userId) {
+            await prisma.pool.update({
+                where: {
+                    id: pool.id,
+                },
+                data: {
+                    userId: request.user.sub
+                }
+            })
+        }
+        // se tudo estiver correto ira seguir para a seguinte rota
+        await prisma.participants.create({
+            data: {
+                poolId: pool.id,
+                userId: request.user.sub,
+            }
 
-
+        })
+        return reply.status(201).send()
     })
-
 }
