b12bb1c1823def2e9f8507aac0ff1bbcf4bee14c
alex miranda de souza <59890212+AlexSouza645@users.noreply.github.com>
2023-12-08 10:41:33 -0300

    estilizacao menu navegacao

--- nlw-copa-mobile/src/routes/app.routes.tsx
+++ nlw-copa-mobile/src/routes/app.routes.tsx
@@ -1,28 +1,45 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { New } from '../screens/New';
 import { Pools } from '../screens/Pools';
-import {PlusCircle, SoccerBall} from 'phosphor-react-native'
+import { PlusCircle, SoccerBall } from 'phosphor-react-native'
 import { useTheme } from 'native-base';
+import { Platform } from 'react-native';
 
 // desestruturar a importação
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppRoutes() {
-    
-    const {colors}= useTheme( )
+
+    const { colors, sizes } = useTheme()
+    const size = sizes[6]
     return (
 
         <Navigator screenOptions={{
-            headerShown:false,
+            headerShown: false,
+            tabBarLabelPosition:'beside-icon',
             tabBarActiveTintColor: colors.yellow[500],
-            tabBarInactiveTintColor:colors.gray[300]
+            tabBarInactiveTintColor: colors.gray[300],
+            // estilização da barra de navegacao
+            tabBarStyle: {
+                position: 'absolute',
+                height: sizes[12],
+                borderTopWidth: 0,
+                backgroundColor: colors.gray[800]
+            },
+            // estilizar os itens da barra
+            tabBarItemStyle:{
+                position:'relative',
+                top:Platform.OS ==='android' ? -10 : 0
+            }
         }}>
             <Screen
 
                 name='new'
                 component={New}
                 options={{
-                tabBarIcon:({color}) => <PlusCircle color={color}/>
+                    tabBarIcon: ({ color }) => <PlusCircle color={color} size={size} 
+                    />,
+                    tabBarLabel:'Novo Bolão '
                 }}
 
 
@@ -34,8 +51,10 @@ export function AppRoutes() {
                 name='Pools'
                 component={Pools}
                 options={{
-                    tabBarIcon:() => <SoccerBall/>
-                    }}
+                    tabBarIcon: () => <SoccerBall 
+                    size={size}  />,
+                    tabBarLabel:'Meus Bolões '
+                }}
 
             />
         </Navigator>
