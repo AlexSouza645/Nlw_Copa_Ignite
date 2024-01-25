3aee196d54f28966a1abde208cf4551b29b24bcf
alex miranda de souza <59890212+AlexSouza645@users.noreply.github.com>
2024-01-25 10:47:04 -0300

    finalização aula 4

--- nlw-copa-mobile/src/routes/index.tsx
+++ nlw-copa-mobile/src/routes/index.tsx
@@ -2,20 +2,23 @@ import { NavigationContainer } from '@react-navigation/native'
 import { AppRoutes } from './app.routes'
 import { SignIn } from '../screens/SignIn'
 import { useAuth } from '../hooks/useAuth'
+import { Box } from 'native-base'
 
 
 export function Routes() {
-   
-   const {user}= useAuth()
-    return (
-        <NavigationContainer>
 
-          {
-            user.name ? <AppRoutes/> : <SignIn/>
-          }
+  const { user } = useAuth()
+  return (
+    <Box flex={1} bg="gray.900">
+      <NavigationContainer>
 
-        </NavigationContainer>
-    )
+        {
+          user.name ? <AppRoutes /> : <SignIn />
+        }
+
+      </NavigationContainer>
+    </Box>
+  )
 
 
 
